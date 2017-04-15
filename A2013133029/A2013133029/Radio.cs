fe8e030eb161ba596883738bb342a29ecc0b1e82
A2013133029 <alvin_diaz@usmp.pe>
2017-04-15 17:20:14 -0500

    SEGUNDO COMMIT DE PRUEBA

---
+++ A2013133029/A2013133029/Radio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2013133029
+{
+    class Radio
+    {
+        public string name { get; set; }
+    }
+}
