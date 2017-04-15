a4d49e8fee74bb7fa7be148453b6080758a2770b
A2013133029 <alvin_diaz@usmp.pe>
2017-04-15 17:10:50 -0500

    MENSAJE DE PRUEBA

---
+++ A2013133029/A2013133029/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2013133029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MENSAJE DE PRUEBA");
+        }
+    }
+}
